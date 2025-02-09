4b20d91b83a47ca95535ce4b91113c1b350184bf
Bret Wortman <bret@thewortmans.org>
2025-02-08 20:16:09 -0500

    Type hints

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import sys
-from cx_Freeze import setup, Executable
+
+from cx_Freeze import Executable, setup
+from setuptools import find_packages
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
-  executables=[
-    Executable("run_curator.py", base=base, target_name="curator"),
-    Executable("run_singleton.py", base=base, target_name="curator_cli"),
-    Executable("run_es_repo_mgr.py", base=base, target_name="es_repo_mgr"),
-  ]
+    packages=find_packages(),
+    executables=[
+        Executable("run_curator.py", base=base, target_name="curator"),
+        Executable("run_singleton.py", base=base, target_name="curator_cli"),
+        Executable("run_es_repo_mgr.py", base=base, target_name="es_repo_mgr"),
+    ],
 )
