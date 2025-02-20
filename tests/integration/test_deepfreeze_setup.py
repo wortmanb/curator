ab59c2cfdc0b33feb40615c01413ce1e7d88640c
Bret Wortman <bret@thewortmans.org>
2025-02-20 09:30:31 -0500

    Temporarily removed this test
    It requires a valid license to run, and I'm not sure how to handle that
    globally.

--- tests/integration/test_deepfreeze_setup.py
+++ tests/integration/test_deepfreeze_setup.py
@@ -22,6 +22,10 @@ INTERVAL = 1  # Because we can't go too fast or cloud providers can't keep up.
 class TestDeepfreezeSetup(DeepfreezeTestCase):
     def test_setup(self):
         for provider in PROVIDERS:
+            warnings.filterwarnings(
+                "ignore", category=DeprecationWarning, module="botocore.auth"
+            )
+
             self.provider = provider
             do_setup(self.client, provider)
             csi = self.client.cluster.state(metric=MET)[MET]["indices"]
@@ -62,55 +66,60 @@ class TestDeepfreezeSetup(DeepfreezeTestCase):
             s3 = s3_client_factory(provider)
             s3.delete_bucket(testvars.df_bucket_name)
 
-    def test_setup_with_ilm(self):
-        warnings.filterwarnings(
-            "ignore", category=DeprecationWarning, module="botocore.auth"
-        )
-        for provider in PROVIDERS:
-            self.provider = provider
-            self.do_setup(create_ilm_policy=True)
-            csi = self.client.cluster.state(metric=MET)[MET]["indices"]
+    # ? This test requires a valid license for the ILM policy to be created.
+    # ? It is disabled by default.
+    # def test_setup_with_ilm(self):
+    #     warnings.filterwarnings(
+    #         "ignore", category=DeprecationWarning, module="botocore.auth"
+    #     )
+    #     for provider in PROVIDERS:
+    #         self.provider = provider
+    #         self.do_setup(create_ilm_policy=True)
+    #         csi = self.client.cluster.state(metric=MET)[MET]["indices"]
 
-            # Specific assertions
-            # Settings index should exist
-            assert csi[STATUS_INDEX]
-            # Settings doc should exist within index
-            assert self.client.get(index=STATUS_INDEX, id=SETTINGS_ID)
-            # Settings index should only have settings doc (count == 1)
-            assert 1 == self.client.count(index=STATUS_INDEX)["count"]
-            # Repo should exist
-            assert self.client.snapshot.get_repository(
-                name=f"{testvars.df_repo_name}-000001"
-            )
-            # Bucket should exist
-            s3 = s3_client_factory(provider)
-            assert s3.bucket_exists(testvars.df_bucket_name)
-            # ILM policy should exist
-            assert self.client.ilm.get_lifecycle(name=testvars.df_ilm_policy)
-            # We can't test the base path on AWS because it won't be created until the
-            #  first object is written, but we can test the settings to see if it's correct
-            #  there.
-            s = self.get_settings()
-            assert s.base_path_prefix == testvars.df_base_path
-            assert s.last_suffix == "000001"
-            assert s.canned_acl == testvars.df_acl
-            assert s.storage_class == testvars.df_storage_class
-            assert s.provider == "aws"
-            assert s.rotate_by == testvars.df_rotate_by
-            assert s.style == testvars.df_style
-            assert s.repo_name_prefix == testvars.df_repo_name
-            assert s.bucket_name_prefix == testvars.df_bucket_name
+    #         # Specific assertions
+    #         # Settings index should exist
+    #         assert csi[STATUS_INDEX]
+    #         # Settings doc should exist within index
+    #         assert self.client.get(index=STATUS_INDEX, id=SETTINGS_ID)
+    #         # Settings index should only have settings doc (count == 1)
+    #         assert 1 == self.client.count(index=STATUS_INDEX)["count"]
+    #         # Repo should exist
+    #         assert self.client.snapshot.get_repository(
+    #             name=f"{testvars.df_repo_name}-000001"
+    #         )
+    #         # Bucket should exist
+    #         s3 = s3_client_factory(provider)
+    #         assert s3.bucket_exists(testvars.df_bucket_name)
+    #         # ILM policy should exist
+    #         assert self.client.ilm.get_lifecycle(name=testvars.df_ilm_policy)
+    #         # We can't test the base path on AWS because it won't be created until the
+    #         #  first object is written, but we can test the settings to see if it's correct
+    #         #  there.
+    #         s = self.get_settings()
+    #         assert s.base_path_prefix == testvars.df_base_path
+    #         assert s.last_suffix == "000001"
+    #         assert s.canned_acl == testvars.df_acl
+    #         assert s.storage_class == testvars.df_storage_class
+    #         assert s.provider == "aws"
+    #         assert s.rotate_by == testvars.df_rotate_by
+    #         assert s.style == testvars.df_style
+    #         assert s.repo_name_prefix == testvars.df_repo_name
+    #         assert s.bucket_name_prefix == testvars.df_bucket_name
 
-            # Clean up
-            self.client.snapshot.delete_repository(
-                name=f"{testvars.df_repo_name}-000001"
-            )
-            self.client.indices.delete(index=STATUS_INDEX)
-            s3 = s3_client_factory(provider)
-            s3.delete_bucket(testvars.df_bucket_name)
+    #         # Clean up
+    #         self.client.snapshot.delete_repository(
+    #             name=f"{testvars.df_repo_name}-000001"
+    #         )
+    #         self.client.indices.delete(index=STATUS_INDEX)
+    #         s3 = s3_client_factory(provider)
+    #         s3.delete_bucket(testvars.df_bucket_name)
 
     def test_setup_bucket_exists(self):
         for provider in PROVIDERS:
+            warnings.filterwarnings(
+                "ignore", category=DeprecationWarning, module="botocore.auth"
+            )
             self.provider = provider
             s3 = s3_client_factory(provider)
             s3.create_bucket(testvars.df_bucket_name)
