4b1bf2472d1273db93fcff11394e8ace59e3c8e8
Bret Wortman <bret@thewortmans.org>
2025-02-20 07:41:38 -0500

    Templating

--- tests/integration/test_deepfreeze_remount.py
+++ tests/integration/test_deepfreeze_remount.py
@@ -1,3 +1,10 @@
 """
 Integration tests for the Remount action
 """
+
+from tests.integration import CuratorTestCase
+
+
+class TestDeepfreezeRemount(CuratorTestCase):
+    def test_remount():
+        pass
--- tests/integration/test_deepfreeze_status.py
+++ tests/integration/test_deepfreeze_status.py
@@ -2,3 +2,10 @@
 Integration tests for Status. Not sure how to handle this since all output is to the terminal...
 
 """
+
+from tests.integration import CuratorTestCase
+
+
+class TestDeepfreezeStatus(CuratorTestCase):
+    def test_status():
+        pass
